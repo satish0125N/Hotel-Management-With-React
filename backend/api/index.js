6ffa7b3bfc36c69a9e5363e754e9712f7fadadb7
Satish zala <satish@glopbe.com>
2025-07-17 16:52:07 +0530

    saprate user.js api call

--- backend/api/index.js
+++ backend/api/index.js
@@ -7,15 +7,15 @@ app.get('/', (req, res) => {
 	res.send('API is working with Vercel Serverless!');
 });
 
-app.get('/users', async (req, res) => {
-	try {
-		const users = await db.user.findMany();
-		res.json(users);
-	} catch (error) {
-		console.error('Error fetching users:', error);
-		res.status(500).json({ error: 'Internal Server Error' });
-	}
-});
+// app.get('/users', async (req, res) => {
+// 	try {
+// 		const users = await db.user.findMany();
+// 		res.json(users);
+// 	} catch (error) {
+// 		console.error('Error fetching users:', error);
+// 		res.status(500).json({ error: 'Internal Server Error' });
+// 	}
+// });
 
 // Local development check
 if (process.env.NODE_ENV !== 'production') {
